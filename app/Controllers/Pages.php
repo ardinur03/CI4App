c8a51b5044b80290915ec9ea34b9556f3a62e97e
ardinur03 <ardinurinsan03@gmail.com>
2020-09-01 17:46:22 +0700

    make variable method name

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -8,21 +8,29 @@ class Pages extends BaseController
 
   public function index()
   {
-    $data = ['title' => 'Home'];
+    $data = [
+      'title' => 'Home',
+      'methodName' => '/'
+    ];
     return view('pages/v_home', $data);
   }
 
   //--------------------------------------------------------------------
   public function about()
   {
-    $data = ['title' => 'About'];
+    $data = [
+      'title' => 'About', 
+      'methodName' => '/about'
+    ];
     return view('pages/v_about', $data);
   }
   //--------------------------------------------------------------------
 
   public function contact()
   {
-    $data = ['title' => 'Contact'];
+    $data = [
+      'title' =>'Contact',
+      'methodName' => '/contact'];
     return view('pages/v_contact', $data);
   }
 
