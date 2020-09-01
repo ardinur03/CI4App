086ab4d9e83cef7b469be926dc3b27b59c52fe62
ardinur03 <ardinurinsan03@gmail.com>
2020-09-01 17:48:03 +0700

    make view contact

--- app/Views/pages/v_contact.php
+++ app/Views/pages/v_contact.php
@@ -2,24 +2,42 @@
 
 <?= $this->section('content'); ?>
 <?= $this->include('layout/navbar'); ?>
-  
-<!--contact section start-->
-<section class="contact-section">
-  <div class="contact-info">
-    <div><i class="fas fa-map-marker-alt"></i>Address, City, Country</div>
-    <div><i class="fas fa-envelope"></i>contact@email.com</div>
-    <div><i class="fas fa-phone"></i>+00 0000 000 000</div>
-    <div><i class="fas fa-clock"></i>Mon - Fri 8:00 AM to 5:00 PM</div>
-  </div>
-  <div class="contact-form">
-    <h2>Contact Me</h2>
-    <form class="contact" action="" method="post">
-      <input type="text" name="name" class="text-box" placeholder="Your Name" required>
-      <input type="email" name="email" class="text-box" placeholder="Your Email" required>
-      <textarea name="message" rows="5" placeholder="Your Message" required></textarea>
-      <input type="submit" name="submit" class="send-btn" value="Send">
-    </form>
+
+<section id="hero-contact">
+  <div class="jumbotron jumbotron-fluid bg-img-jumbotron justify-content-center">
+    <div class="container">
+      <h1 class="display-4 contact-me">Contact Me</h1>
+    </div>
   </div>
 </section>
+
+<!--contact section start-->
+<section id="contact-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-5">
+        <div class="contact-info">
+          <div><i class="fas fa-map-marker-alt"></i>Jalan Raya Contong No. 63</div>
+          <div><i class="fas fa-envelope"></i>ardinurinsan03@gmail.com</div>
+          <div><i class="fas fa-phone"></i>0895328885809</div>
+          <div><i class="fas fa-clock"></i><p id="jam"></p> : <p id="menit"></p> : <p id="detik"></p></div>
+        </div>
+      </div>
+      <div class="col-md-7">
+        <div class="contact-form">
+          <h2>Contact Me</h2>
+          <form class="contact" action="" method="post">
+            <input type="text" name="name" class="text-box" placeholder="Your Name" required>
+            <input type="email" name="email" class="text-box" placeholder="Your Email" required>
+            <textarea name="message" rows="5" placeholder="Your Message" required></textarea>
+            <input type="submit" name="submit" class="send-btn" value="Send">
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section> <br><br><br>
 <!--contact section end-->
+
+<?= $this->include('layout/footer'); ?>
 <?= $this->endsection(); ?>
