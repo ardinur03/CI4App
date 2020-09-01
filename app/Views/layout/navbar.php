daea61303672e884a0bd4488337ca229320fbd9d
ardinur03 <ardinurinsan03@gmail.com>
2020-09-01 17:47:13 +0700

    logika if navbar active

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,7 +1,7 @@
 <header id="navbar-section">
   <div class="fixed-top">
-    <!-- // ($title != 'Contact') ? 'bg-tranparent' : 'bg-secondary';  -->
-    <nav class=" navbar navbar-expand-lg navbar-dark bg-tranparent" id="mainNav">
+    <!-- // ($title != 'Contact') ? 'bg-tranparent' : 'bg-secondary'; animate__rubberBand -->
+    <nav class=" navbar navbar-expand-lg navbar-dark <?= ($methodName == '/poliklinik') ? 'bg-primary' : 'bg-tranparent';; ?> animate__animated animate__zoomIn animate__delay-1s" id="mainNav">
       <div class="container">
         <a class="navbar-brand abc" href="/">Muhamad Ardi Nur Insan</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,16 +9,16 @@
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul class="navbar-nav p-1">
-            <li class="nav-item active">
+            <li class="nav-item <?= ($methodName == '/') ? 'active present' : ''; ?>">
               <a class="nav-link js-scroll-trigger abc" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?= ($methodName == '/about') ? 'active present' : ''; ?>">
               <a class="nav-link js-scroll-trigger abc" href="/about">About</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?= ($methodName == '/contact') ? 'active present' : ''; ?>">
               <a class="nav-link js-scroll-trigger abc" href="/contact">Contact</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?= ($methodName == '/poliklinik') ? 'active' : ''; ?>">
               <a class="nav-link js-scroll-trigger abc" href="/poliklinik">Poliklinik</a>
             </li>
           </ul>
