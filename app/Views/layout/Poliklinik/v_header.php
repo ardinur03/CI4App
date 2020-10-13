a1be57d275876504ece330586a7b6bb06db6fcbd
ardinur03 <ardinurinsan03@gmail.com>
2020-10-13 14:02:19 +0700

    update title

--- app/Views/layout/Poliklinik/v_header.php
+++ app/Views/layout/Poliklinik/v_header.php
@@ -27,6 +27,6 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/Poliklinik" class="brand-link">
-      <img src="<?= base_url(); ?>/assets/img/icon-img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Portofolio</span>
+      <img src="<?= base_url(); ?>/assets/img/icon-img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3 mt-1" style="opacity: .8">
+      <span class="brand-text font-weight-light judul">Poliklinik</span>
     </a>
