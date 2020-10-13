c4488ef0f56623875057850c07579bccf910ef50
ardinur03 <ardinurinsan03@gmail.com>
2020-10-13 14:01:48 +0700

    add js

--- app/Views/layout/Poliklinik/v_footer.php
+++ app/Views/layout/Poliklinik/v_footer.php
@@ -30,12 +30,15 @@
 
 <!-- jQuery -->
 <script src="<?= base_url('assets/poliklinik/plugins/jquery/jquery.min.js'); ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="<?= base_url('assets/poliklinik/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 <!-- AdminLTE App -->
 <script src="<?= base_url('assets/poliklinik/dist/js/adminlte.min.js'); ?>"></script>
+<!-- sweet alert -->
+<script src="<?= base_url('assets/js/sweetalert2.all.min.js'); ?>"></script>
 <!-- Custom JS -->
-<link rel="stylesheet" href="<?= base_url('assets/poliklinik/js/custom.js') ?>">
+<script src="<?= base_url('assets/js/custom.js') ?>"></script>
 </body>
 
 </html>
