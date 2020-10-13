609705a4a6eecb10c00b7c4b86cf43304eeedc64
ardinur03 <ardinurinsan03@gmail.com>
2020-10-13 14:02:36 +0700

    create js

---
+++ app/Views/layout/Poliklinik/v_js.php
@@ -0,0 +1,14 @@
+<!-- jQuery -->
+<script src="<?= base_url('assets/poliklinik/plugins/jquery/jquery.min.js'); ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= base_url('assets/poliklinik/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url('assets/poliklinik/dist/js/adminlte.min.js'); ?>"></script>
+<!-- sweet alert -->
+<script src="<?= base_url('assets/js/sweetalert2.all.min.js'); ?>"></script>
+<!-- Custom JS -->
+<script src="<?= base_url('assets/js/custom.js') ?>"></script>
+</body>
+
+</html>
